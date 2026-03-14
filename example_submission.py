e9c0c8dbc67634e728bd72bb6cb39a0c7e0be10d
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2026-03-14 21:42:03 +0100

    test data chunking

--- example_submission.py
+++ example_submission.py
@@ -14,8 +14,8 @@ ENDPOINT = "task2"
 API_TOKEN = os.getenv("TEAM_TOKEN")
 SERVER_URL = os.getenv("SERVER_URL")
 # Change accordingly
-JSONL_FILE = "predictions/python-practice-chunk.jsonl"
-STAGE = "practice"
+JSONL_FILE = "predictions/python-test-chunk.jsonl"
+STAGE = "public"
 
 def main():
     if not API_TOKEN:
